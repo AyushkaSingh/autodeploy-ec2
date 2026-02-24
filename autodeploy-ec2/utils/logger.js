e0e83c8c51b64ba5dbc7c09663bcfa95a8db6bb4
Ayushka <149486303+AyushkaSingh@users.noreply.github.com>
2026-02-24 12:51:54 +0530

    Delete autodeploy-ec2 directory

--- autodeploy-ec2/utils/logger.js
+++
@@ -1,8 +0,0 @@
-import chalk from "chalk";
-
-export const log = {
-  info: (msg) => console.log(chalk.blue(msg)),
-  success: (msg) => console.log(chalk.green(msg)),
-  error: (msg) => console.log(chalk.red(msg)),
-  warn: (msg) => console.log(chalk.yellow(msg)),
-};
